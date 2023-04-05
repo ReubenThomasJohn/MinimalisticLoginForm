4b0eab1cfe7414b18ba73c135ab8e896b9486434
ReubenThomasJohn <reubenthomas.john2017@gmail.com>
2023-04-05 17:17:29 +0530

    css styling for password matching added

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ A simple, minimalist login + registration form built using Node.js, Express, Mon
 
 TODO:
 
-- [ ] Add CSS styling to indicate the re-typed password does not match the password in the registration form.
+- [x] Add CSS styling to indicate the re-typed password does not match the password in the registration form.
 - [ ] Upon pressing register button, email needs to be sent with a 'confirm email' prompt: Done to validate mail ID input.
---
+++ public/src/registerPasswordValidation.js
@@ -0,0 +1,13 @@
+function validatePassword() {
+  let password1 = document.getElementById('p1');
+  let password2 = document.getElementById('p2');
+  console.log(password2.value);
+  if (password1.value === password2.value) {
+    // document.getElementById('password1').style.color = 'green';
+    document.getElementById('submitButton').disabled = false;
+    password2.style.color = 'green';
+  }
+  //   document.getElementById('password1').style.color = 'red';
+  else password2.style.color = 'red';
+  //   document.getElementById('submitButton').disabled = false;
+}
