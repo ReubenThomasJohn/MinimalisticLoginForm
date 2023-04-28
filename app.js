fbc4f07300a6e2f0b4c71d2f5d90acd113b48b30
ReubenThomasJohn <reubenthomas.john2017@gmail.com>
2023-04-28 13:54:18 +0530

    adding username to google strategy, while saving to DB

--- app.js
+++ app.js
@@ -69,9 +69,12 @@ passport.use(
     },
     function (accessToken, refreshToken, profile, cb) {
       console.log(profile);
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });
+      User.findOrCreate(
+        { username: profile.displayName, googleId: profile.id },
+        function (err, user) {
+          return cb(err, user);
+        }
+      );
     }
   )
 );
